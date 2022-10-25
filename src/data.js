3c0865d273e1aa45692b755a67c839b5d1626d5b
hevedrios <exxed.ridel@gmail.com>
2022-10-24 20:18:03 -0500

    Hotfix: Named data array to fix Netlify warning

--- src/data.js
+++ src/data.js
@@ -1,12 +1,13 @@
-export default [
+const data = [
   {
     id: "1",
     title: "Web App - Bajo la Música™ Magazine",
     linkName: "https://bajolamusica.netlify.app",
     linkUrl: "https://bajolamusica.netlify.app",
     image: "https://images2.imgbox.com/8c/e8/zmeQdMJ0_o.jpg",
-    description: "This will be the official Bajo la Musica™ web app in which local artists can get their music to know. Website is been created using React framework and will escalate into a React Native App. Navbar was created following a Figma wireframe but all other UI/UX by myself. This project is on a test phase to be released soon.",
-    proyectUrl: "https://github.com/exxedridel/BajoLaMusicaPJ"
+    description:
+      "This will be the official Bajo la Musica™ web app in which local artists can get their music to know. Website is been created using React framework and will escalate into a React Native App. Navbar was created following a Figma wireframe but all other UI/UX by myself. This project is on a test phase to be released soon.",
+    proyectUrl: "https://github.com/exxedridel/BajoLaMusicaPJ",
   },
   {
     id: "2",
@@ -14,8 +15,9 @@ export default [
     linkName: "http://hevedrios.pythonanywhere.com",
     linkUrl: "http://hevedrios.pythonanywhere.com",
     image: "https://images2.imgbox.com/90/d6/3IxOtbkv_o.jpg",
-    description: "This is the web app I delivered to Manufacturera San Roque™ as result of my professional practices, which is an employee management system plus rewards store. This one was developed in Python's Flask framework and SQLite as database. User Interface was created using Bootstrap. You can use this credentials to full navigate - user: Heved123, password: 123456.",
-    proyectUrl: "https://github.com/exxedridel/SanRoquePJ"
+    description:
+      "This is the web app I delivered to Manufacturera San Roque™ as result of my professional practices, which is an employee management system plus rewards store. This one was developed in Python's Flask framework and SQLite as database. User Interface was created using Bootstrap. You can use this credentials to full navigate - user: Heved123, password: 123456.",
+    proyectUrl: "https://github.com/exxedridel/SanRoquePJ",
   },
   {
     id: "3",
@@ -23,7 +25,10 @@ export default [
     linkName: "https://hevedrios.netlify.app",
     linkUrl: "https://hevedrios.netlify.app",
     image: "https://images2.imgbox.com/70/cb/L6jx4EqV_o.jpg",
-    description: "This site has been built with npx create-react-app. Templates were made from scratch with JS-HTML-CSS and UI is responsive design.",
-    proyectUrl: "https://github.com/exxedridel/my-portfolio"
+    description:
+      "This site has been built with npx create-react-app. Templates were made from scratch with JS-HTML-CSS and UI is responsive design.",
+    proyectUrl: "https://github.com/exxedridel/my-portfolio",
   },
 ];
+
+export default data;
